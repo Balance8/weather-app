ca77663f88fb92b8ad5e6684b2a4eabfb62f5c33
Balance8 <mslocum819@gmail.com>
2019-12-12 18:44:55 -0800

    detailed summary request

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const request = require('request');
 
 const url =
-  'https://api.darksky.net/forecast/3b22ee6d97112360bbfc136bdb59240e/37.8267,-122.4233';
+  'https://api.darksky.net/forecast/3b22ee6d97112360bbfc136bdb59240e/33.976,-118.4484';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body;
+  console.log(
+    data.daily.data[0].summary,
+    `Currently it is ${data.currently.temperature} degrees out there.  There is a ${data.currently.precipProbability}% chance of rain`
+  );
 });
