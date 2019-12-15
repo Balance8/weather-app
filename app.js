95cd3710f2901ea6afeb06e2886dfdb8e15d0ed4
Balance8 <mslocum819@gmail.com>
2019-12-14 18:21:09 -0800

    forecast util

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-const request = require('request');
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const darkSky =
-  'https://api.darksky.net/forecast/3b22ee6d97112360bbfc136bdb59240e/33.976,-118.4484';
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
 
-request({ url: darkSky, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to weather service!');
-  } else if (response.body.error) {
-    console.log('Unable to find Location');
-  } else {
-    const data = response.body;
-    console.log(
-      data.daily.data[0].summary,
-      `Currently it is ${data.currently.temperature} degrees out there.  There is a ${data.currently.precipProbability}% chance of rain`
-    );
-  }
+geocode('marina del rey', (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
 });
-
-// Geocoding
-// Address -> Lat/long -> Weather
-
-const mapBox =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Marina%20Del%20Rey.json?access_token=pk.eyJ1IjoiYmFsYW5jZTgiLCJhIjoiY2s0M242MTMzMDlyMzNscXkxMGdpbXBoMyJ9.1a-gsPnSO1stB0NkDPrbeg&limit=1';
-
-request({ url: mapBox, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to Geolocation Service');
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find Location');
-  } else {
-    const longitude = response.body.features[0].center[0];
-    const latitude = response.body.features[0].center[1];
-    console.log(longitude, latitude);
-  }
-});
-
-geocode('boston', (error, data) => {
+forecast(33.9774, -118.4508, (error, data) => {
   console.log('Error', error);
   console.log('Data', data);
 });
