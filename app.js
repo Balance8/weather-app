ef36455464485186bc5d4dfe886107f7adb50634
Balance8 <mslocum819@gmail.com>
2019-12-14 18:48:06 -0800

    call back chaining

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+const address = process.argv[2];
 
-geocode('marina del rey', (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
-forecast(33.9774, -118.4508, (error, data) => {
-  console.log('Error', error);
-  console.log('Data', data);
-});
+if (!address) {
+  console.log('Please provide a location');
+} else {
+  geocode(address, (error, data) => {
+    if (error) return console.log('Error', error);
+
+    console.log('Data', data);
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) return console.log('Error', error);
+
+      console.log(data.location);
+      console.log(forecastData.currently);
+    });
+  });
+}
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,5 +1,15 @@
 const request = require('request');
 
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
